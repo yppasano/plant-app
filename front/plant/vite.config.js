bd2a687797a972bbe0723429f07470ca4401defc
yp-asano <asano@ype-design.jp>
2026-03-02 18:10:57 +0900

    解析画面追加

--- front/plant/vite.config.js
+++ front/plant/vite.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        analytics: 'analytics.html'
+      }
+    }
+  },
   server: {
     port: 3000,
     host: true,
